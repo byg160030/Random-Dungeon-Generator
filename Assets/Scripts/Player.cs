8afbc2da4d70d92cf25f3bb440f5f1f8c916f85f
Brice Yuuki Gibson <byg160030@utdallas.edu>
2021-08-14 19:01:12 -0500

    Player Movement (Flip X)
    Player is now able to flip the player character to left to right.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Transform GFX;
+	float flipX;
+
+	// Use this for initialization
+	void Start () {
+		GFX = GetComponentInChildren<SpriteRenderer>().transform;
+		flipX = GFX.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horz =  System.Math.Sign(Input.GetAxisRaw("Horizontal"));
+		if(Mathf.Abs(horz) > 0)
+        {
+			GFX.localScale = new Vector2(flipX * horz, GFX.localScale.y);
+        }
+	}
+}
